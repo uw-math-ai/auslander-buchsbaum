d01773945e270c9b0be7af28246af39ce0f4f9a7
Leopold Mayer <leomayer@uw.edu>
2026-03-02 21:03:21 -0800

    made a basic definition of determinant and some sorries to fill in

--- Mathlib/RingTheory/RegularLocalRing/FreeResolution.lean
+++ Mathlib/RingTheory/RegularLocalRing/FreeResolution.lean
@@ -3,6 +3,11 @@ module
 public import Mathlib.Algebra.Homology.QuasiIso
 public import Mathlib.Algebra.Homology.SingleHomology
 public import Mathlib.Algebra.Category.ModuleCat.Basic
+public import Mathlib.LinearAlgebra.Dimension.Finrank
+public import Mathlib.LinearAlgebra.ExteriorAlgebra.Basic
+public import Mathlib.RingTheory.PicardGroup
+public import Mathlib.RingTheory.Finiteness.Basic
+public import Mathlib.Algebra.Homology.ShortComplex.ShortExact
 
 /-!
 # Projective resolutions
@@ -43,3 +48,53 @@ along with a quasi-isomorphism to the complex consisting of just `Z` supported i
 instance : CategoryWithHomology (ModuleCat R) := sorry
 
 example (com : ChainComplex (ModuleCat R) ℕ) : ∀ i : ℕ, HasHomology com i := inferInstance
+
+
+/-
+  Define the determinant of a module, show that the determinant is additive
+-/
+section Determinant
+
+variable (R M : Type*) [CommRing R] [AddCommGroup M] [Module R M]
+
+open Module TensorProduct
+
+noncomputable
+def det := ⋀[R]^(finrank R M) M
+
+--TODO: add finiteness, projectiveness hypotheses so this is actually true
+def det_additive {S : ShortComplex (ModuleCat R)} (hS : S.ShortExact) :
+    (det R S.X₁) ⊗[R] (det R S.X₂) ≃ₗ[R] det R S.X₂ := sorry
+
+end Determinant
+
+
+/-
+  I want to express that if an invertible module has a finite free resolution,
+  then it is actually free
+-/
+section FiniteFreeRes
+
+open Module
+
+#check Module.Free.tensor
+#check Module.dual_free
+
+variable (R M : Type*) [CommRing R] [AddCommGroup M] [Module R M]
+
+structure FiniteFreeResolution (n : ℕ) where
+  /-- the chain complex involved in the resolution -/
+  complex : ChainComplex (ModuleCat R) ℕ
+  acyclic : complex.Acyclic
+  /-- the chain complex must be degreewise projective -/
+  zero : complex.X 0 = M
+  finite : ∀ n > 0, Module.Finite R (complex.X n)
+  free : ∀ n > 0, Free R (complex.X n)
+  bounded : ∃ N, ∀ n > N, IsZero (complex.X n)
+
+variable (S : FiniteFreeResolution R M 5)
+
+theorem free_of_finite_free_res (hM : Module.Invertible R M) {n : ℕ}
+    (h : Nonempty (FiniteFreeResolution R M n)) : Free R M := sorry
+
+end FiniteFreeRes
